d8ec213b6193bee92e5d55d0e3e8bd49c321c1de
Konolin <mandru.catalin03@gmail.com>
2023-08-29 11:29:25 +0300

    Small bishop fix

--- src/com/chess/engine/piece/Bishop.java
+++ src/com/chess/engine/piece/Bishop.java
@@ -19,7 +19,7 @@ public class Bishop extends Piece {
     }
 
     @Override
-    public Collection<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(final Board board) {
         final List<Move> legalMoves = new ArrayList<>();
 
         for (final int candidateCoordinateOffset : CANDIDATE_MOVES_VECTOR_COORDINATES) {
---
+++ src/com/chess/engine/piece/Rook.java
@@ -0,0 +1,13 @@
+package com.chess.engine.piece;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.Collection;
+
+public class Rook extends Piece {
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        return null;
+    }
+}
