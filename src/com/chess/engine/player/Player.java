a6b5794e6867ebd7fe8836e0d85ccdd23374fb60
Konolin <mandru.catalin03@gmail.com>
2023-08-31 13:58:16 +0300

    Player pt 2

--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -1,5 +1,6 @@
 package com.chess.engine.player;
 
+import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.piece.King;
@@ -27,5 +28,38 @@ public abstract class Player {
         throw new RuntimeException("Not a valid board, king is missing!");
     }
 
+    public boolean isMoveLegal(final Move move) {
+        return this.legalMoves.contains(move);
+    }
+
+    public boolean isInCheck() {
+        return false;
+        // TODO - implementation
+    }
+
+    public boolean isInCheckMate() {
+        return false;
+        // TODO - implementation
+    }
+
+    public boolean isInStalemate() {
+        return false;
+        // TODO - implementation
+    }
+
+    public boolean isCastled() {
+        return false;
+        // TODO - implementation
+    }
+
+    public MoveTransition makeMove(final Move move) {
+        return null;
+        // TODO - implementation
+    }
+
     public abstract Collection<Piece> getActivePieces();
+
+    public abstract Alliance getAlliance();
+
+    public abstract Player getOpponent();
 }
