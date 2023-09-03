1effd7099ae7d0b7581ed1d538b305b062cc5083
Konolin <mandru.catalin03@gmail.com>
2023-09-03 13:01:54 +0300

    GUI pt 4

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -1,18 +1,16 @@
 package com.chess.engine.board;
 
-import jdk.jfr.Description;
-
 public class BoardUtils {
     public static final boolean[] FIRST_COLUMN = initColumn(0);
     public static final boolean[] SECOND_COLUMN = initColumn(1);
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
-    public static final boolean[] FIRST_ROW = initRow(0);;
-    public static final boolean[] SECOND_ROW = initRow(8);
-    public static final boolean[] THIRD_ROW = initRow(16);
-    public static final boolean[] FIFTH_ROW = initRow(32);
-    public static final boolean[] SEVENTH_ROW = initRow(48);
+    public static final boolean[] SECOND_ROW = initRow(48);
+    public static final boolean[] THIRD_ROW = initRow(32);
+    public static final boolean[] FIFTH_ROW = initRow(16);
+    public static final boolean[] SEVENTH_ROW = initRow(8);
+    public static final boolean[] EIGHTH_ROW = initRow(0);;
 
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
