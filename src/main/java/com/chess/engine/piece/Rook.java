92fef95a378fe185eb9676a8b1a36bb28b61ed31
Catalin <catalin.mandru@stud.ubbcluj.ro>
2023-12-17 13:45:00 +0200

    Rook legal moves remake

--- src/main/java/com/chess/engine/piece/Rook.java
+++ src/main/java/com/chess/engine/piece/Rook.java
@@ -9,24 +9,29 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
-import com.chess.engine.board.Tile;
+import com.chess.engine.board.MoveUtils;
+import com.chess.engine.board.MoveUtils.Line;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Rook extends Piece {
     /**
-     * offsets for the rook's possible moves
+     * The candidate move vector coordinates for a rook.
      */
     private final static int[] CANDIDATE_MOVES_VECTOR_COORDINATES = {-8, -1, 1, 8};
 
     /**
+     * The pre-computed legal moves for each tile coordinate on a chessboard for a rook
+     */
+    private final static Map<Integer, Line[]> PRECOMPUTED_LEGAL_MOVES = computeLegalMoves();
+
+    /**
      * Constructor for a rook that initializes its position, alliance, and type.
      *
-     * @param pieceAlliance   the alliance of the rook
-     * @param piecePosition   the position of the rook
+     * @param pieceAlliance the alliance of the rook
+     * @param piecePosition the position of the rook
      */
     public Rook(final Alliance pieceAlliance, final int piecePosition) {
         super(piecePosition, pieceAlliance, PieceType.ROOK, true);
@@ -35,9 +40,9 @@ public class Rook extends Piece {
     /**
      * Constructor for a rook that initializes its position, alliance, type, and whether it is its first move.
      *
-     * @param pieceAlliance   the alliance of the rook
-     * @param piecePosition   the position of the rook
-     * @param isFirstMove     whether it is the rook's first move
+     * @param pieceAlliance the alliance of the rook
+     * @param piecePosition the position of the rook
+     * @param isFirstMove   whether it is the rook's first move
      */
     public Rook(final Alliance pieceAlliance, final int piecePosition, final boolean isFirstMove) {
         super(piecePosition, pieceAlliance, PieceType.ROOK, isFirstMove);
@@ -46,8 +51,8 @@ public class Rook extends Piece {
     /**
      * Helper method that checks if the rook is in the first or eighth column and the destination offset vector is invalid.
      *
-     * @param currentPosition      the current position of the rook
-     * @param candidateOffset       the destination offset vector
+     * @param currentPosition the current position of the rook
+     * @param candidateOffset the destination offset vector
      * @return {@code true} if the rook is in the first or eighth column and the destination offset vector is invalid
      */
     private static boolean isFirstOrEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
@@ -56,43 +61,57 @@ public class Rook extends Piece {
     }
 
     /**
+     * Initializes and returns a mapping of tile coordinates to legal moves for the rook.
+     *
+     * @return An immutable mapping of tile coordinates to legal moves for the rook
+     */
+    private static Map<Integer, Line[]> computeLegalMoves() {
+        Map<Integer, Line[]> legalMoves = new HashMap<>();
+        for (int position = 0; position < BoardUtils.NUM_TILES; position++) {
+            List<Line> lines = new ArrayList<>();
+            for (int offset : CANDIDATE_MOVES_VECTOR_COORDINATES) {
+                Line line = new Line();
+                int destination = position;
+                while (BoardUtils.isValidCoordinate(destination)) {
+                    if (isFirstOrEighthColumnExclusion(destination, offset)) {
+                        break;
+                    }
+                    destination += offset;
+                    if (BoardUtils.isValidCoordinate(destination)) {
+                        line.addCoordinate(destination);
+                    }
+                }
+                if (!line.isEmpty()) {
+                    lines.add(line);
+                }
+            }
+            if (!lines.isEmpty()) {
+                legalMoves.put(position, lines.toArray(new Line[0]));
+            }
+        }
+        return ImmutableMap.copyOf(legalMoves);
+    }
+
+    /**
      * Calculates the legal moves for a rook on the board.
      *
-     * @param board     the board on which the rook is placed
-     * @return {@code Collection<Move>} a collection of all the legal moves for the rook
+     * @param board the board on which the rook is placed
+     * @return a collection of all the legal moves for the rook
      */
     @Override
     public Collection<Move> calculateLegalMoves(final Board board) {
         final List<Move> legalMoves = new ArrayList<>();
-
-        for (final int candidateCoordinateOffset : CANDIDATE_MOVES_VECTOR_COORDINATES) {
-            int candidateDestinationCoordinate = this.piecePosition;
-
-            // while next position is in-bounds
-            while (BoardUtils.isValidCoordinate(candidateDestinationCoordinate)) {
-                // jump over the cases where the destination offset vector is not valid
-                if (isFirstOrEighthColumnExclusion(candidateDestinationCoordinate, candidateCoordinateOffset)) {
-                    break;
-                }
-
-                // calculate next position, check if in-bounds and decide the move type
-                candidateDestinationCoordinate += candidateCoordinateOffset;
-                if (BoardUtils.isValidCoordinate(candidateDestinationCoordinate)) {
-                    final Tile candidateDestinationTile = board.getTileAtCoordinate(candidateDestinationCoordinate);
-
-                    if (!candidateDestinationTile.isTileOccupied()) {
-                        // make normal move
-                        legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
-                    } else {
-                        final Piece pieceAtDestination = candidateDestinationTile.getPieceOnTile();
-                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
-
-                        if (this.pieceAlliance != pieceAlliance) {
-                            // make attacking move if next tile is occupied by opponent piece
-                            legalMoves.add(new Move.MajorAttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
-                        }
-                        break; // stop rook from moving further after capturing or being blocked by another piece
+        for (final MoveUtils.Line line : PRECOMPUTED_LEGAL_MOVES.get(this.piecePosition)) {
+            for (final int candidateDestinationCoordinate : line.getLineCoordinates()) {
+                final Piece pieceAtDestination = board.getPiece(candidateDestinationCoordinate);
+                if (pieceAtDestination == null) {
+                    legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+                } else {
+                    if (this.pieceAlliance != pieceAtDestination.getPieceAlliance()) {
+                        legalMoves.add(new Move.MajorAttackMove(board, this, candidateDestinationCoordinate,
+                                pieceAtDestination));
                     }
+                    break;
                 }
             }
         }
@@ -102,7 +121,7 @@ public class Rook extends Piece {
     /**
      * Moves the rook to the destination coordinate and returns a new rook.
      *
-     * @param move  the move to be made
+     * @param move the move to be made
      * @return {@code Rook} a new rook at the destination coordinate
      */
     @Override
